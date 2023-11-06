323446754dce123ec0530f23ba42a45017c78d47
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-11-06 20:24:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setuptools.setup(
     name="PyBingScrapper",
-    version="1.0.0",
+    version="1.0.1",
     author="Vishnu Nandakumar",
     author_email="nkumarvishnu25@gmail.com",
     description="Python wrapper for Bing Search and RAG extraction",
